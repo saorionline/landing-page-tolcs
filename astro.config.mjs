d3acc83b68a8ef6a3e713d7281196fc7dfca792d
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-07-03 20:30:22 -0500

    Vue Successfully Integrated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import vue from '@astrojs/vue';
 //import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [tailwind(), mdx(), react()],
+  integrations: [tailwind(), mdx(), react(), vue()],
   markdown: {
     rehypePlugins: [
       // ... other rehype plugins
